c33d9a5f3d0ecc56d9f83b786bf98680d58cc90e
BT_1N3_32@bktech.local <BT_1N3_32@bktech.local>
2018-12-04 14:47:03 -0500

    Initial commit

---
+++ src/Game/Board.java
@@ -0,0 +1,14 @@
+package Game;
+
+import Rooms.Room;
+import People.Person;
+
+public class Board {
+    private Room[][] map;
+    private int length;
+    private int width;
+
+    public Board {
+
+    }
+}
